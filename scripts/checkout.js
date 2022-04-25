44dd2cdcc15af2dd132e646361666dced9de2a34
moh shahrukh khan <shahrukhturk9@gmail.com>
2022-04-25 13:22:23 +0530

    .

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,36 +1,36 @@
 // Each ticket will cost 100 Rupees
 // If wallet amount is insufficient show alert "Insufficient Balance!";
 // Else show "Booking successfull!" and adjust the wallet amount in real time
-let amount= localStorage.getItem("amount")|| 0
+let amount = localStorage.getItem("amount") || 0
 
-document.getElementById("wallet").innerText=amount
+document.getElementById("wallet").innerText = amount
 
-let Box=document.getElementById("movie")
+let Box = document.getElementById("movie")
 
-let moviesData=JSON.parse(localStorage.getItem("movies"))
+let moviesData = JSON.parse(localStorage.getItem("movie"))
 
-function confirmfun(){
-  let  numberSeats=document.getElementById("number_of_seats").value
-let total=Number(numberSeats)*100
+function confirmfun() {
+    let numberSeats = document.getElementById("number_of_seats").value
+    let total = Number(numberSeats) * 100
 
-if(amount<total){
-    alert("Insufficient Balance!")
-}else{
-    alert("Booking successfull")
+    if (amount < total) {
+        alert("Insufficient Balance!")
+    } else {
+        alert("Booking successfull")
 
-    let updat=amount - total
-    localStorage.setItem("amount",updat)
-    window.location.reload()
+        let updat = amount - total
+        localStorage.setItem("amount", updat)
+        window.location.reload()
+    }
 }
-}
-let div =document.createElement("div")
+let div = document.createElement("div")
 
-let img=document.createElement("img")
+let img = document.createElement("img")
 
-img.src=moviesData.Poster
-  title=document.createElement("p")
-  title.innerText=moviesData.
-  
-  div.append(title,img)
+img.src = moviesData.Poster;
+title = document.createElement("p")
+title.innerText = moviesData.Title;
 
-  Box.append(div)
+div.append(title, img)
+
+Box.append(div)
--- scripts/movies.js
+++ scripts/movies.js
@@ -4,44 +4,47 @@
 let amount = localStorage.getItem("amount") || 0
 
 document.getElementById("wallet").innerText = amount
-async function myfun() {
-    const api = "62075466"
-
-    try {
-        let q = document.getElementById("search").value;
-
-        const res = await fetch(`https://www.omdbapi.com/?s=${q}&apikey="e18b3001"`)
-        // console.log(res)
-        const data = await res.json()
-        // console.log(data.Search)
-        appendData(data.Search)
-
-    } catch (err) {
-        console.log(err)
-    }
-
+function myfun(){
+    let q = document.getElementById("search").value;
+   
+    fetch(`https://www.omdbapi.com/?s=${q}&apikey=e18b3001`)
+        .then(function(res){
+            // console.log(res);
+            return res.json();
+        }).then(function(res){
+            // console.log(res)
+            appendData(res.Search);
+        })
+        .catch(function(err){
+            console.log(err);
+        })
 }
+
 
 
 function appendData(data) {
     document.getElementById("movies").innerHTML = null
 
+    if(data == undefined){
+        return false;
+    }
 
 
 
-
-    data.forEach((el) => {
-        console.log(el)
+    data.forEach(function(el) {
+        // console.log(el)
 
         let div = document.createElement("div")
         let img = document.createElement("img")
 
-        img.src = el.Poster
+        img.src = el.Poster;
         title = document.createElement("p")
         title.innerText = el.Title
         btn = document.createElement("button")
         btn.setAttribute("class", "book_now")
-        btn.addEventListener("click", (el) => {
+        btn.innerText="Book Now";
+        btn.addEventListener("click", function() {
+            // console.log(el)
             CheckoutPage(el)
         })
         div.append(img, title, btn)
@@ -55,13 +58,14 @@ function appendData(data) {
 
 function CheckoutPage(el) {
     localStorage.setItem("movie", JSON.stringify(el))
+    // console.log(el)
     window.location.href = "checkout.html"
 }
 
 
 
 let id;
-function debounce(func, delay) {
+function debounce(delay) {
     if (id) {
         clearTimeout(id)
     }
