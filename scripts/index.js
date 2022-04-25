75c9ec54b731c4ac0e7da3ae98093140902dd7ce
moh shahrukh khan <shahrukhturk9@gmail.com>
2022-04-25 12:57:46 +0530

    .

--- scripts/index.js
+++ scripts/index.js
@@ -12,15 +12,17 @@
 <button id="book_movies">Book Movies</button>
 <!-- button with id as "book_movies" to go to movies.html --> */}
 
-let amount= localStorage.getItem("amount")|| 0
+let amount= localStorage.getItem("amount")|| 0 
 
 document.getElementById("wallet").innerText=amount
 
 function myfunction(){
-    let addAmount=document.getElementById("wallet").value
-    let nowAmount= Number(amount) + Number(addAmount)
-    console.log(nowAmount)
-document.getElementById("wallet").innerText= nowAmount
+    // console.log(amount);
+    let addAmount=document.getElementById("amount").value;
+    // console.log(addAmount)
+    let nowAmount= Number(amount) + Number(addAmount);
+    // console.log(nowAmount)
+document.getElementById("wallet").innerText= nowAmount;
 localStorage.setItem("amount",nowAmount)
 window.location.reload()
 
--- scripts/movies.js
+++ scripts/movies.js
@@ -1,71 +1,71 @@
 // Implement debouncing for network request
 // On clicking book now store the selected movie in localstorage as key "movie"
 // so that you can retrive it on checkout.html 
-let amount= localStorage.getItem("amount")|| 0
+let amount = localStorage.getItem("amount") || 0
 
-document.getElementById("wallet").innerText=amount
-async function myfun(){
-  const api="e18b3001"
-     
-        try{
-        let    q=document.getElementById("search").value
-        
-            const res=await fetch(`https://wwww.omdbapi.com/?s=${q}&apikey=${api}`)
+document.getElementById("wallet").innerText = amount
+async function myfun() {
+    const api = "62075466"
 
-            const {Search}=await res.json()
-            
-            appendData(Search)
+    try {
+        let q = document.getElementById("search").value;
 
-        }catch(err){
-            console.log(err)
-        }
+        const res = await fetch(`https://www.omdbapi.com/?s=${q}&apikey="e18b3001"`)
+        // console.log(res)
+        const data = await res.json()
+        // console.log(data.Search)
+        appendData(data.Search)
+
+    } catch (err) {
+        console.log(err)
+    }
 
 }
 
 
-function appendData(data){
-    document.getElementById("movies").innerHTML=null
-
-    document.createElement(tag)
+function appendData(data) {
+    document.getElementById("movies").innerHTML = null
 
 
 
-data.forEach( (el)=>{
-    console.log(el)
 
-  let  div=document.createElement("div")
-   let img=document.createElement("img")
 
-  img.src=el.Poster
-    title=document.createElement("p")
-    title.innerText=el.Title
-    btn=document.createElement("button")
-    btn.setAttribute("class","book_now")
-    btn.addEventListener("click",(el)=>{
-      CheckoutPage(el)
+    data.forEach((el) => {
+        console.log(el)
+
+        let div = document.createElement("div")
+        let img = document.createElement("img")
+
+        img.src = el.Poster
+        title = document.createElement("p")
+        title.innerText = el.Title
+        btn = document.createElement("button")
+        btn.setAttribute("class", "book_now")
+        btn.addEventListener("click", (el) => {
+            CheckoutPage(el)
+        })
+        div.append(img, title, btn)
+
+
+        document.getElementById("movies").append(div)
+
+
     })
-  div.append(img,title,btn)
+}
 
-
-    document.getElementById("movies").append(div)
-
-
-})
-    }
-
-    function CheckoutPage(el){
-        localStorage.setItem("movie",JSON.stringify(el))
-        window.location.href="checkout.html"
-    }
+function CheckoutPage(el) {
+    localStorage.setItem("movie", JSON.stringify(el))
+    window.location.href = "checkout.html"
+}
 
 
 
 let id;
-function debounce(func,delay){
-    if(id){
-    clearTimeout(id)
+function debounce(func, delay) {
+    if (id) {
+        clearTimeout(id)
     }
-    id=setTimeout(function(){
+    id = setTimeout(function () {
         myfun()
-    },delay);
+    }, delay);
 }
